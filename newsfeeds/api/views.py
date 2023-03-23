9555ee937c0b7cd8cde784ee4a9100add1ca7350
Wang <guannan.wang@intel.com>
2023-03-23 19:44:47 +0800

    newsfeeds pagination

--- newsfeeds/api/views.py
+++ newsfeeds/api/views.py
@@ -5,10 +5,12 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import permission_classes
+from utils.paginations import EndlessPagination
 
 
 class NewsFeedViewSet(viewsets.GenericViewSet):
     permission_classes = [IsAuthenticated]
+    pagination_class = EndlessPagination
 
     def get_query_set(self):
         # 当前查看newsfeed是有权限的，只能查看当前登录用户的newsfeed
@@ -19,5 +21,7 @@ class NewsFeedViewSet(viewsets.GenericViewSet):
         ).order_by('-created_at')
 
     def list(self, request):
-        serializer = NewsFeedSerializer(self.get_query_set(), context={'request': request}, many=True)
-        return Response({'newsfeeds': serializer.data}, status=status.HTTP_200_OK)
+        queryset = self.get_query_set()
+        page = self.paginate_queryset(queryset)
+        serializer = NewsFeedSerializer(page, context={'request': request}, many=True)
+        return self.get_paginated_response(serializer.data)
