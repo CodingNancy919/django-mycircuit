96f83624dc60bca7e3c7d87663c6e9e611429077
Wang, Guannan <guannan.wang@intel.com>
2023-04-16 20:55:20 +0800

    install celery and add newsfeed fanout task using message queue

--- twitter/cache.py
+++ twitter/cache.py
@@ -5,4 +5,3 @@ USER_TWEETS_PATTERN = 'user_tweets:{user_id}'
 USER_NEWSFEEDS_PATTERN = 'user_newsfeeds:{user_id}'
 # 一般follower会不断更新，并且对于有些人可能有千万级别的粉丝，没有必要缓存 然而following量级不会特别大，也不会频繁更新因此一般缓存following
 #redis
-
